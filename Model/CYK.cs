4c8f469e8cfc860f57c6b15f7a74f274174ed671
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2021-11-04 08:27:37 -0500

    squeleton, need write all methods

---
+++ Model/CYK.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cyk_grammar_tester.Model
+{
+    class CYK
+    {
+        private string[,] Table { get; set; }
+        private CYK uniqueInstance;
+
+        private CYK()
+        {
+
+        }
+
+        public CYK GetInstance()
+        {
+            return uniqueInstance;
+        }
+
+        public void AddInTable(int row, int col, string value)
+        {
+
+        }
+        public bool IsProduceByGrammar()
+        {
+            return false;
+        }
+    }
+}
---
+++ Model/Probador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cyk_grammar_tester.Model
+{
+    class Probador
+    {
+        private string[,] Grammar { get; set; }
+        private string TestString { get; set; }
+        private CYK cyk;
+
+        public Probador()
+        {
+
+        }
+
+        public void SetCYK(CYK cyk)
+        {
+
+        }
+
+        public void GenerateTable()
+        {
+
+        }
+    }
+}

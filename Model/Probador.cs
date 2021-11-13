ae9524b4bf159eb555abb26549c60af8a278ffb7
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2021-11-13 11:34:29 -0500

    method getvariablesProduce completed

--- Model/Probador.cs
+++ Model/Probador.cs
@@ -86,11 +86,51 @@ namespace cyk_grammar_tester.Model
         /// <param name="b">The first variables of the algorithm<./param>
         /// <param name="c">The second variables of the algorithm.</param>
         /// <returns>
-        ///     The grammar's variables have a production found with the algorithm. with the same format of the table's value.
+        ///     The grammar's variables have a production found with the algorithm. 
+        ///     with the same format of the table's value. if has no valiables with the productions returns empty
         /// </returns>
         public string GetVariablesProduce(string b, string c)
         {
-            return b + c;
+            string[] firstVariables = b.Split(',');
+            string[] secondVariables = c.Split(',');
+            string combination = "";
+            foreach (string variable1 in firstVariables)
+            {
+                for(int i = 0; i < secondVariables.Length; i++)
+                {
+                    combination += variable1 + secondVariables[i] + ",";
+                }
+            }
+            string[] searchProductions = combination.Split(',');
+            string value = "";
+            foreach (string production in searchProductions)
+            {
+                for (int row = 0; row < Grammar.GetLength(0); row++)
+                {
+                    string[] grammarProductions = Grammar[row, 1].Split(',');
+                    bool found = false;
+                    for (int i = 0; i < grammarProductions.Length && !found; i++)
+                    {
+                        if (production.Equals(grammarProductions[i]))
+                        {
+                            found = true;
+                            if (!value.Contains(Grammar[row, 0]))
+                            {
+                                value += Grammar[row, 0] + ",";
+                            }
+                        }
+                    }
+                }
+                if(value.Split(',').Length == Grammar.GetLength(0))
+                {
+                    break;
+                }
+            }
+            if (value[value.Length - 1] == ',')
+            {
+                value = value.Substring(0, value.Length - 1);
+            }
+            return value;
         }
     }
 }
